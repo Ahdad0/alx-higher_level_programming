6b247685af9a670ed427628be1a2f7f5d52c07af
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-03 21:34:54 +0100

    donr

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        n = 0
+        if i <= 'z' and i >= 'a':
+            n = 32
+        print(("{:c}".format(ord(i)-n)), end="")
+    print()
