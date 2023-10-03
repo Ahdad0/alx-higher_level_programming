16d06fe5ec93dcb4789f6ff3ed15c60c2063fae5
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-03 21:10:22 +0100

    task passed

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c <= 'z' and c >= 'a':
+    s = ord(c)
+    if s < 123 and s > 96:
         return True
-    else:
-        return False
+    return False
