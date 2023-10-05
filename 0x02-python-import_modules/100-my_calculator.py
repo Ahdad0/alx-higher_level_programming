89389146aa9eb262becf12990ebb159bece0f361
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-05 21:20:40 +0100

    DONE

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
-o, a, b = 0, 0, 0
-ope = ""
-
-arg = len(sys.argv) - 1
-
-for i in sys.argv:
-    if (o == 1):
-        a = int(i)
-    elif (o == 2):
-        ope = i
-    elif (o == 3):
-        b = int(i)
-    o += 1
-
 if __name__ == "__main__":
-    if arg != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) - 1 != 3:
+     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+     sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    av = sys.argv[2]
+    operator = {"+": add, "-": sub, "*": mul, "/": div}
+    if av not in list(operator.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    else:
-        match ope:
-            case "+":
-                print("{} {} {} = {}".format(a, ope, b, add(a, b)))
-            case "-":
-                print("{} {} {} = {}".format(a, ope, b, sub(a, b)))
-            case "*":
-                print("{} {} {} = {}".format(a, ope, b, mul(a, b)))
-            case "/":
-                print("{} {} {} = {}".format(a, ope, b, div(a, b)))
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit(1)
+    print("{} {} {} = {}".format(a, av, b, operator[av](a, b)))
