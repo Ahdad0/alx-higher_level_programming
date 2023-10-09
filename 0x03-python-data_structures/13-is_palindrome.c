37140f1f8eb7e0c2ec307b95c0a5e5254b45222e
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-09 12:28:19 +0100

    task done

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,27 +12,39 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *after = NULL;
 	listint_t *curr = NULL;
-	listint_t *check = NULL;
+	int store_f[10], store_l[10], i = 0, u = 0, o = 0;
 
 	curr = *head;
 	after = *head;
-	check = *head;
 
+	after = after->next;
+
+	while (after->n != curr->n)
+	{
+		store_f[i] = curr->n;
+		after = after->next;
+		curr = curr->next;
+		i++;
+	}
+
+	store_f[i] = curr->n;
 	while (after->next != NULL)
 	{
+		store_l[u] = after->n;
 		after = after->next;
+		u++;
 	}
 
-	curr = curr->next;
+	store_l[u] = after->n;
 
-	while (check->next->next != NULL)
+	while (u >= 0 && i >= o)
 	{
-		check = check->next;
-	}
-
-	if (after->n == (*head)->next->n && curr->n == check->n)
-	{
-		return (0);
+		if (store_f[o] != store_l[u])
+		{
+			return (0);
+		}
+		o++;
+		u--;
 	}
 
 	return (1);
