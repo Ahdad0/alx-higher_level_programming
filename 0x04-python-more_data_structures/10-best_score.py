d9c6a91a164b63decdba9057f14f32c8ca063f94
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-11 14:18:33 +0100

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    first = 0
+    for k, v in a_dictionary.items():
+        if first < v:
+            first = v
+            key = k
+    return key
