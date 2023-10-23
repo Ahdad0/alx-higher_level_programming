4fa937953155afd949f340f3070c6e2eafac083c
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-23 20:30:29 +0100

    task done

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
+
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as e:
-        print("Exception:", e)
+        sys.stderr.write("Exception:" + str(e) + '\n')
         return False
