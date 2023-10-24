ffa7b1fc7f58e8a1a940492a492eb7b3aaf90317
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-24 17:34:09 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""define a class square"""
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, value):
+        """init:
+
+            Args:
+                size (int): size of the square
+        """
+        self.__size = value
