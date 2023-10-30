79a2550c702adbf5e5cc0ca652e3e4b852e02cb7
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-10-30 23:45:11 +0100

    added

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Unittest for max_integer([..])
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_area(self):
+        self.assertAlmostEqual(max_integer([2, 5, 7]), 7)
+        self.assertAlmostEqual(max_integer([-2, -4, -1]), -1)
+        self.assertAlmostEqual(max_integer([7]), 7)
+        self.assertAlmostEqual(max_integer([7, 8.3]), 8.3)
+        self.assertAlmostEqual(max_integer([]), None)
+
+    def test_values(self):
+        self.assertRaises(TypeError, max_integer, [2, 's', 3])
+        self.assertRaises(TypeError, max_integer, ['s', 3])
+        self.assertRaises(TypeError, max_integer, ['2', 3])
+        self.assertRaises(TypeError, max_integer, [False, 3])
+
+if __name__ == "__main__":
+    unittest.main()
