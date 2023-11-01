d2276c027135974e73b020b58c63ff1adfd46def
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-01 21:41:03 +0100

    task 6 updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,7 +4,6 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
 
     """run test with python3 -m unittest -v tests.6-max_integer_test"""
