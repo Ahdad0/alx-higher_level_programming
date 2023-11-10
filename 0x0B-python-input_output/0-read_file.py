54ff5490308f4a4f8c6df8b5fdf188f4bf327a63
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-10 10:57:42 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ read and print contents from a file
 
 def read_file(filename=""):
     """read and print contents from a text"""
-    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
