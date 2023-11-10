ab761ee0aa70e4091bf8a6c1549392a4d570e843
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-10 12:44:28 +0100

    task 7 updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,9 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-
 try:
-    existing = load_from_json_file(filename)
+    existing = load_from_json_file('add_item.json')
 except FileNotFoundError:
-    existing_content = []
+    existing = []
 
-save_to_json_file(existing_content + argv[1:], filename)
+save_to_json_file(existing + argv[1:], 'add_item.json')
