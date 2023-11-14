d724202444d8e5e3241be5d34ef7a7331c7612f3
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-14 13:08:04 +0100

    tasj 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,6 +33,17 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        """Getter size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter size - sets width and height as size"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """print to stdout"""
         return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
