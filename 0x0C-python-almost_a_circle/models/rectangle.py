fb867583b1ff43dd74547f33a7f2be8072c65bd4
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-14 11:18:52 +0100

    fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,8 @@ Inherits : from Base;
 Inits    : superclass' id
 Contains : private width, height, x, y
 """
+
+
 from models.base import Base
 
 
@@ -25,7 +27,7 @@ class Rectangle(Base):
         y(self)          y(self, value)
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """__init__"""
+        """Init"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -55,19 +57,35 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         """setter height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
         """setter x"""
-        self__x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @y.setter
     def y(self, value):
         """setter y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
