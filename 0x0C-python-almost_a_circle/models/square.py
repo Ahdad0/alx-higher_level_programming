058ce919263835fe4de33083ac748cba6c009441
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-14 13:06:22 +0100

    task 9

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module contains a class Square
+
+Inherits : from Rectangle;
+Inits    : superclass' id, width (as size), height (as size), x, y
+Contains : public attribute size
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    defines a class Square; inherits from class Rectangle
+    Inherited Attributes:
+        id
+        __weight        __height
+        __x             __y
+    Class Attributes:
+        size
+    Inherted Methods:
+        width(self)      width(self, value)
+        height(self)     height(self, value)
+        x(self)          x(self, value)
+        y(self)          y(self, value)
+    Methods:
+        __str__
+        __init__(self, size, x=0, y=0, id=None)
+        size(self)       size(self, value)
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """init"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """print to stdout"""
+        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
+                self.id, self.x, self.y, self.size)
