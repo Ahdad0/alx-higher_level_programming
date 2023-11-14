ce06d5df751793c7c7c9ea843c002fc775b43830
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-14 13:37:55 +0100

    task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
-Base class
+Module contains a class Base
+
+Contains: private class __nb_objects, and class constructor __init__
+Returns : JSON string representation of list dictionaries
+Saves   : JSON strings of instance dictionaries into file
 """
+import json
 
 
 class Base:
     """
-    Method:
-        public class attribute: __nb_objects
+    defines a class Base
+    Class Attributes:
+        __nb_objects
+    Methods:
         __init__(self, id=None)
+    Static Methods:
+    to_json_string(list_dictionaries)   from_json_string(json_string)
     """
     __nb_objects = 0
 
@@ -19,3 +28,10 @@ class Base:
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns JSON str representation of list dict"""
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        else:
+            return '[]'
