f45e3b67e4c17638d02bb67ed245604eb38f2eba
Ahdad0 <abderrahimahdadabdo@gmail.com>
2024-02-12 20:59:19 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,11 +2,19 @@
 
 const first = process.argv[2];
 const num = parseInt(first);
+let list = '';
 
 if (isNaN(num)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < first; i++) {
-    console.log('XX');
+    for (let j = 0; j < first; j++) {
+      list += 'X';
+    }
+    if (i + 1 < first) {
+      list += '\n';
+    } else {
+      console.log(list);
+    }
   }
 }
