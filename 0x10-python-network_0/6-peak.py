42ae3a1d52bc69c4c416de958ff2dde4832e239d
Ahdad0 <abderrahimahdadabdo@gmail.com>
2024-03-05 20:42:25 +0100

    task done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""define a function"""
+
 
 def find_peak(list_of_integers):
+    """find the peak in the list"""
     lenght = len(list_of_integers)
     if lenght == 0:
         return None
